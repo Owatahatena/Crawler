99d8880398f01a737087c0f95c1e0048fb3be14f
owatahatena <owatahatena@gmail.com>
2018-01-21 17:54:10 +0900

    '熊本高専のHPのリンクとタイトルを再帰的に取得するやつ'

---
+++ LinkScrapy/LinkScrapy/spiders/linkspider.py
@@ -0,0 +1,21 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+from LinkScrapy.items import LinkscrapyItem
+
+class LinkSpider(CrawlSpider):
+    name = 'geturl'
+    allwed_domains = ['www.kumamoto-nct.ac.jp']
+    start_urls = ['http://www.kumamoto-nct.ac.jp']
+
+    rules = [Rule(LinkExtractor(), callback='parse_link', follow=True)]
+
+
+    def parse_link(self, respose):
+        sel = Selector(respose)
+        item = LinkscrapyItem()
+        item['url'] = respose.url
+
+        item['title'] = sel.xpath('/html/head/title/text()').extract()
+
+        return item
